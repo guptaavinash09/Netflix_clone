4471747cb709b33c1d7a4d285b2f3c4c044819a2
Avinash Gupta <avinashkumargupta306@gmail.com>
2025-07-03 01:10:36 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,18 @@ function App() {
       <MovieRow title="🎬 Action Hits" movies={movies} />
     </div>
   );
-}
+
 
 export default App;
+
+
+
+function App() {
+  return (
+    <div className="bg-zinc-900 min-h-screen text-white">
+      <Navbar />
+      <MovieRow title="🔥 Trending Now" movies={movies} />
+      <MovieRow title="⭐ Top Rated" movies={movies} />
+      <MovieRow title="🎬 Action Hits" movies={movies} />
+    </div>
+  );
